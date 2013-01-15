12668390533f84ff0ed119786ee5d3666c877cf8
Ed Chen <edychen@gmail.com>
2013-01-15 18:02:26 +0000

    Added pages for login and login error.
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@1726 998562b1-c221-4fe5-b3d3-2fd2107a5f19

---
+++ src/main/webapp/error.jsp
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Enrichr</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="author" content="Edward Y. Chen"/>
+	<meta name="viewport" content="width=750">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+	<link rel="icon" type="image/ico" href="favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/reset/reset-min.css"/>
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" type="text/css" href="black-widow.css"/>
+	<link rel="stylesheet" type="text/css" href="black-widow-login.css"/>
+</head>
+<body>
+	<div id="logo">
+		<a href="index.html"><img src="images/enrichr-icon.png"/><span>En</span><span class="red">rich</span><span>r</span></a>
+	</div>
+	<div class="clear"></div>
+	<div id="navbar" class="nav">
+		<table>
+			<tr>
+				<td>
+					<a href="login.html">Login</a>
+				</td>
+				<td>
+					<a href="login.html#register">Register</a>
+				</td>
+				<td>
+					<a href="login.html#forgot">Forgot Your Password?</a>
+				</td>				
+			</tr>
+		</table>
+	</div>
+	<div id="content" class="beveled">
+		<div class="title">Error</div>
+		<div><%=request.getAttribute("error")%></div>
+	</div>
+</body>
+</html>
---
+++ src/main/webapp/login.js
@@ -0,0 +1,129 @@
+/* Navigation */
+function navigateTo(index, transitionSpeed) {
+	if (globals.changingCategory || $('div.selected').index() == index)
+		return;
+	else
+		globals.changingCategory = true;
+	transitionSpeed = (typeof transitionSpeed === 'undefined') ? 'slow' : transitionSpeed;
+	$('#content div.selected').fadeToggle(transitionSpeed, function() {
+		$('.selected').removeClass('selected');
+		$('#navbar td').eq(index).addClass('selected');
+		$('#content > div').eq(index).addClass('selected');
+		$('#content div.selected').fadeToggle(transitionSpeed);
+		globals.changingCategory = false;
+	});
+}
+
+function getTab(name) {
+	window.location.hash = name;
+}
+
+/* Form data handling */
+function validateLogin() {
+	if ($('form#register input[name=email]').val().trim() == '')
+		alert('You must specify an email address.');
+}
+
+function validateRegister() {
+	if ($('form#register input[name=email]').val().trim() == '')
+		alert('You must specify an email address.');
+	else if ($('form#register input[name=password]').val() != $('form#register input[name=confirm]').val())
+		alert('Passwords don\'t match.');
+	else
+		return true;
+
+	return false;
+}
+
+function hashcheck(onload) {
+	var transitionSpeed = (typeof onload !== 'boolean') ? 'slow' : 0;
+
+	var hash = window.location.hash.substring(1);
+	if (hash[0] == '!') {
+		// Not on find tab
+		if (globals.tabList[$('div.selected').index()] != 'find') {
+			navigateTo(globals.tabList.indexOf('find'), transitionSpeed);
+			if (!onload)
+				return;
+		}
+
+		// Query gene
+		hash = hash.substring(1);
+		var parms = hash.split('=');
+		if (parms[0] == 'gene')
+			queryGene(parms[1]);
+	}
+	else {
+		navigateTo(globals.tabList.indexOf(hash), transitionSpeed);
+	}
+}
+
+$(document).ready(function () {
+	$.ajaxSetup({ cache: false });	// Prevent IE from caching GET requests
+	globals = {};	// Stores global vars
+	globals.changingCategory = false;	// Prevent changing category too fast
+	globals.tabList = ['', 'register', 'forgot'];
+
+	// Onload hash check
+	hashcheck(true);
+
+	// When swipe changes the hash
+	window.onhashchange = hashcheck;
+
+	// Disable user scaling in UIWebView
+	if (/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(navigator.userAgent))
+		$('meta[name=viewport]').attr('content', $('meta[name=viewport]').attr('content') + ', user-scalable=no');
+
+	// Touch gestures
+	$.event.special.swipe.durationThreshold = 200;
+	$('body').swipeleft(function() {
+			var dest = ($('div.selected').index() - 1 + globals.tabList.length) % globals.tabList.length;
+			getTab(globals.tabList[dest]);
+		}
+	);
+	$('body').swiperight(function() {
+			var dest = ($('div.selected').index() + 1) % globals.tabList.length;
+			getTab(globals.tabList[dest]);
+		}
+	);
+
+	// Enable placeholders and error messages for required fields
+	$('input.has-placeholder:text[value=""]').next('span.placeholder-text').show();
+	$('input.has-placeholder').focus(function() {
+		var input = $(this);
+		input.next('span.placeholder-text').hide();
+		if (input.hasClass('required')) {
+			input.siblings('div.errormsg').hide();
+			input.removeClass('error');
+		}
+	}).blur(function() {
+		var input = $(this);
+		if (input.val() == '') {
+			input.next('span.placeholder-text').show();
+			if (input.hasClass('required')) {
+				input.siblings('div.errormsg').show();
+				input.addClass('error');
+			}
+		}
+	});	
+
+	// Error messages for password checking
+	$('form#register input[type=password]').focus(function() {
+		$('form#register label#confirm div.errormsg').hide();
+		$('form#register input[type=password]').removeClass('error');
+	})
+	$('form#register input[name=confirm]').blur(function() {
+		var input = $(this);
+		if ($('form#register input[name=password]').val() != input.val()) {
+			input.siblings('div.errormsg').show();
+			$('form#register input[type=password]').addClass('error');
+		}
+	})
+
+	// // Bind query-gene button to javascript
+	// $('#find-gene').submit(function() {
+	// 	var gene = $('#query-gene').val()
+	// 	window.location.hash = '!gene=' + gene;
+	// 	return false;
+	// });
+});
