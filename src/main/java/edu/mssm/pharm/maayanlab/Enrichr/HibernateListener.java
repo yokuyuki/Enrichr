634caf23d558097dade5f14d00329d2dc854ed7f
Ed Chen <edychen@gmail.com>
2013-03-01 21:48:26 +0000

    HibernateListener creates a hibernate sessionfactory on servlet startup
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@1791 998562b1-c221-4fe5-b3d3-2fd2107a5f19

---
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/HibernateListener.java
@@ -0,0 +1,21 @@
+package edu.mssm.pharm.maayanlab.Enrichr;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import edu.mssm.pharm.maayanlab.HibernateUtil;
+
+@WebListener
+public class HibernateListener implements ServletContextListener {
+	
+	@Override
+	public void contextInitialized(ServletContextEvent event) {         
+		HibernateUtil.getSessionFactory(); // Just call the static initializer of that class        
+	}       
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {         
+		HibernateUtil.getSessionFactory().close(); // Free all resources     
+	}
+}
