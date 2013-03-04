02241ed24387b8b3e4b02e25c0ab24bfb91ebb4b
Ed Chen <edychen@gmail.com>
2013-03-04 19:53:20 +0000

    Bump common-web to 1.1. Add ContextFinalizer to destroy stuck threads.
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@1798 998562b1-c221-4fe5-b3d3-2fd2107a5f19

--- src/main/java/edu/mssm/pharm/maayanlab/Enrichr/ContextFinalizer.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/ContextFinalizer.java
@@ -1,13 +1,20 @@
 package edu.mssm.pharm.maayanlab.Enrichr;
 
+import java.util.Set;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.mssm.pharm.maayanlab.HibernateUtil;
 
 @WebListener
-public class HibernateListener implements ServletContextListener {
+public class ContextFinalizer implements ServletContextListener {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ContextFinalizer.class);
 	
 	@Override
 	public void contextInitialized(ServletContextEvent event) {         
@@ -17,5 +24,15 @@ public class HibernateListener implements ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {         
 		HibernateUtil.getSessionFactory().close(); // Free all resources
+		
+		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();        
+		for (Thread t : threadSet) {
+			if (t.getName().contains("Abandoned connection cleanup thread")) {
+                synchronized(t) {
+                	LOGGER.warn("Forcibly stopping thread to avoid memory leak: " + t.getName());
+                    t.stop();	//don't complain, it works
+                }
+            }
+		}
 	}
 }
