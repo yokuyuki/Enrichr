b0b356bbdfd7cd06d8232b095c200a4df001987f
Ed Chen <edychen@gmail.com>
2013-04-30 19:16:20 +0000

    Add xml describing all the gene set libraries, their structure, whether they have a grid, and their format. Use ResourceLoader to parse the xml and create a data structure out of that.
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@1900 998562b1-c221-4fe5-b3d3-2fd2107a5f19

---
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/ResourceLoader.java
@@ -0,0 +1,106 @@
+package edu.mssm.pharm.maayanlab.Enrichr;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.gson.Gson;
+
+public class ResourceLoader {
+
+	private static final ResourceLoader instance; // Singleton
+	private static EnrichmentCategory[] categories;
+
+	public static void main(String args[]) {
+		ResourceLoader loader = ResourceLoader.getInstance();
+		System.out.println(new Gson().toJson(loader.getCategories()));
+	}
+
+	static {
+		instance = new ResourceLoader();
+	}
+
+	public static ResourceLoader getInstance() {
+		return instance;
+	}
+
+	private ResourceLoader() {
+		InputStream is = ResourceLoader.class.getClassLoader()
+				.getResourceAsStream("gene_set_libraries.xml");
+		try {
+			JAXBContext jaxbContext = JAXBContext
+					.newInstance(EnrichmentCategories.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			EnrichmentCategories root = (EnrichmentCategories) jaxbUnmarshaller
+					.unmarshal(is);
+			categories = root.getCategories();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public EnrichmentCategory[] getCategories() {
+		return categories;
+	}
+
+	@XmlRootElement(name = "categories")
+	private static class EnrichmentCategories {
+
+		@XmlElement(name = "category")
+		private EnrichmentCategory[] categories;
+
+		public EnrichmentCategory[] getCategories() {
+			return categories;
+		}
+
+	}
+
+	@XmlRootElement(name = "category")
+	public static class EnrichmentCategory {
+
+		@XmlAttribute
+		private String name;
+
+		@XmlElement(name = "library")
+		private GeneSetLibrary[] libraries;
+
+		public String getName() {
+			return name;
+		}
+
+		public GeneSetLibrary[] getLibraries() {
+			return libraries;
+		}
+	}
+
+	@XmlRootElement(name = "library")
+	public static class GeneSetLibrary {
+
+		@XmlAttribute
+		private String name;
+
+		@XmlAttribute
+		private String format;
+
+		@XmlAttribute
+		private boolean hasGrid;
+
+		public String getName() {
+			return name;
+		}
+
+		public String getFormat() {
+			return format;
+		}
+
+		public boolean hasGrid() {
+			return hasGrid;
+		}
+	}
+
+}
