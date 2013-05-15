f57e420ec810e93be51782c0acc04e58f7de57ad
Ed Chen <edychen@gmail.com>
2013-05-15 22:55:55 +0000

    Computation of enriched terms depends on gene set library type
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@1994 998562b1-c221-4fe5-b3d3-2fd2107a5f19

--- src/main/java/edu/mssm/pharm/maayanlab/Enrichr/GeneSetLibrary.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/GeneSetLibrary.java
@@ -7,9 +7,14 @@
 
 package edu.mssm.pharm.maayanlab.Enrichr;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
+
+import pal.statistics.FisherExact;
+import edu.mssm.pharm.maayanlab.common.math.SetOps;
 
 public class GeneSetLibrary {
 
@@ -72,5 +77,42 @@ public class GeneSetLibrary {
 
 	public boolean isRanked() {
 		return isRanked;
-	}	
+	}
+	
+	// Filter genes from the input list that are not in the gene-set library
+	protected HashSet<String> filterInputGenes(Collection<String> inputGeneList) {
+		HashSet<String> inputGenes = new HashSet<String>();
+		for (String gene : inputGeneList) {
+			gene = gene.toUpperCase();
+			if (this.contains(gene))
+				inputGenes.add(gene);
+		}
+		
+		return inputGenes;
+	}
+	
+	public ArrayList<EnrichedTerm> getEnrichedTerms(Collection<String> inputGeneList) {
+		HashSet<String> inputGenes = filterInputGenes(inputGeneList);
+		
+		ArrayList<EnrichedTerm> enrichedTerms = new ArrayList<EnrichedTerm>();
+		for (Term currentTerm : this.getTerms()) {
+			// Intersection of term's gene set and input genes
+			Set<String> overlap = SetOps.intersection(currentTerm.getGeneSet(), inputGenes); 
+			
+			int numOfTermGenes = currentTerm.getNumOfTermGenes();
+			int numOfBgGenes = this.getNumOfBackgroundGenes();
+			int numOfInputGenes = inputGenes.size();
+			int numOfOverlappingGenes = overlap.size();
+			
+			// Don't bother if there are no target input genes
+			if (numOfOverlappingGenes > 0) {
+				FisherExact fisherTest = new FisherExact(numOfInputGenes + numOfBgGenes);				
+				double pValue = fisherTest.getRightTailedP(numOfOverlappingGenes, numOfInputGenes - numOfOverlappingGenes, numOfTermGenes, numOfBgGenes - numOfTermGenes);
+								
+				enrichedTerms.add(new EnrichedTerm(currentTerm, overlap, pValue));
+			}
+		}
+		
+		return enrichedTerms;
+	}
 }
