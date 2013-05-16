acc651fa8825a83bb2ffb62f194b9e2a03c116e3
Ed Chen <edychen@gmail.com>
2013-05-16 16:52:00 +0000

    Return to computing enrichment in Enrichment.java
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@1997 998562b1-c221-4fe5-b3d3-2fd2107a5f19

--- src/main/java/edu/mssm/pharm/maayanlab/Enrichr/Enrichment.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/Enrichment.java
@@ -12,10 +12,15 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+import pal.statistics.FisherExact;
 
 import edu.mssm.pharm.maayanlab.common.core.FileUtils;
 import edu.mssm.pharm.maayanlab.common.core.Settings;
 import edu.mssm.pharm.maayanlab.common.core.SettingsChanger;
+import edu.mssm.pharm.maayanlab.common.math.SetOps;
 
 public class Enrichment implements SettingsChanger {
 	
@@ -86,11 +91,42 @@ public class Enrichment implements SettingsChanger {
 	}
 	
 	public ArrayList<EnrichedTerm> enrich(GeneSetLibrary geneSetLibrary) {
-		ArrayList<EnrichedTerm> enrichedTerms = geneSetLibrary.getEnrichedTerms(geneList);
+		HashSet<String> inputGenes = filterInputGenes(geneSetLibrary);
+		
+		ArrayList<EnrichedTerm> enrichedTerms = new ArrayList<EnrichedTerm>();
+		for (Term currentTerm : geneSetLibrary.getTerms()) {
+			// Intersection of term's gene set and input genes
+			Set<String> overlap = SetOps.intersection(currentTerm.getGeneSet(), inputGenes); 
+			
+			int numOfTermGenes = currentTerm.getNumOfTermGenes();
+			int numOfBgGenes = geneSetLibrary.getNumOfBackgroundGenes();
+			int numOfInputGenes = inputGenes.size();
+			int numOfOverlappingGenes = overlap.size();
+			
+			// Don't bother if there are no target input genes
+			if (numOfOverlappingGenes > 0) {
+				FisherExact fisherTest = new FisherExact(numOfInputGenes + numOfBgGenes);				
+				double pValue = fisherTest.getRightTailedP(numOfOverlappingGenes, numOfInputGenes - numOfOverlappingGenes, numOfTermGenes, numOfBgGenes - numOfTermGenes);
+								
+				enrichedTerms.add(new EnrichedTerm(currentTerm, overlap, pValue));
+			}
+		}
 		
 		sortEnrichedTerms(geneSetLibrary, enrichedTerms);
 		
 		return enrichedTerms;
+	}
+	
+	// Filter genes from the input list that are not in the gene-set library
+	private HashSet<String> filterInputGenes(GeneSetLibrary geneSetLibrary) {
+		HashSet<String> inputGenes = new HashSet<String>();
+		for (String gene : geneList) {
+			gene = gene.toUpperCase();
+			if (geneSetLibrary.contains(gene))
+				inputGenes.add(gene);
+		}
+		
+		return inputGenes;
 	}
 	
 	// Sort enriched terms
