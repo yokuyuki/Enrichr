262f4827517286da95ee52fa4dbc18eec59289ec
Ed Chen <edychen@gmail.com>
2013-05-20 16:34:48 +0000

    Override term's get enriched term for fuzzy term
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@2015 998562b1-c221-4fe5-b3d3-2fd2107a5f19

--- src/main/java/edu/mssm/pharm/maayanlab/Enrichr/FuzzyTerm.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/FuzzyTerm.java
@@ -3,6 +3,9 @@ package edu.mssm.pharm.maayanlab.Enrichr;
 import java.util.Map;
 import java.util.Set;
 
+import edu.mssm.pharm.maayanlab.common.math.FuzzyEnrichment;
+import edu.mssm.pharm.maayanlab.common.math.SetOps;
+
 public class FuzzyTerm extends Term {
 
 	private Map<String, Double> geneSet;
@@ -28,5 +31,19 @@ public class FuzzyTerm extends Term {
 	public void setGeneSet(Map<String, Double> geneSet) {
 		this.geneSet = geneSet;
 		this.numOfTermGenes = geneSet.size();
+	}
+	
+	@Override
+	public EnrichedTerm getEnrichedTerm(Set<String> inputGenes) {
+		EnrichedTerm enrichedTerm = null;
+		
+		// Intersection of term's gene set and input genes
+		Set<String> overlap = SetOps.intersection(this.getGeneSet(), inputGenes);
+		
+		if (overlap.size() > 0) {
+			return new EnrichedTerm(this, overlap, FuzzyEnrichment.unweightedToWeighted(inputGenes, this.getWeightedGeneSet()));
+		}
+		
+		return enrichedTerm;
 	}
 }
