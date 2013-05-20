167da0f371bfe83b264cf2ac324eeb9783dbc56f
Ed Chen <edychen@gmail.com>
2013-05-20 16:34:30 +0000

    Use fancy integer ceiling function
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@2014 998562b1-c221-4fe5-b3d3-2fd2107a5f19

--- src/main/java/edu/mssm/pharm/maayanlab/Enrichr/FuzzyGeneSetLibrary.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/FuzzyGeneSetLibrary.java
@@ -3,6 +3,8 @@ package edu.mssm.pharm.maayanlab.Enrichr;
 import java.util.Collection;
 import java.util.HashMap;
 
+import edu.mssm.pharm.maayanlab.common.math.NumberUtils;
+
 public class FuzzyGeneSetLibrary extends GeneSetLibrary {
 
 	public FuzzyGeneSetLibrary(Collection<String> libraryLines) {
@@ -20,7 +22,7 @@ public class FuzzyGeneSetLibrary extends GeneSetLibrary {
 			String[] splitLine = line.split("\\t");
 			String termName = splitLine[0];
 			// Initialize HashMap to the rounded up to nearest integer of (splitLine.length-2)/0.75 to make one of sufficient size
-			HashMap<String, Double> geneSet = new HashMap<String, Double>(((splitLine.length-2)*4+2)/3); 
+			HashMap<String, Double> geneSet = new HashMap<String, Double>(NumberUtils.roundUpDivision((splitLine.length-2)*4, 3)); 
 			
 			for (int i = 2; i < splitLine.length; i++) {
 				String[] genePair = splitLine[i].split(",");				
