e5195eb347fed8c163a4285165ee2435d580851f
Ed Chen <edychen@gmail.com>
2013-06-20 21:33:37 +0000

    Add comments
    git-svn-id: https://svn-server/svn/maayanlab/trunk/Enrichr@2110 998562b1-c221-4fe5-b3d3-2fd2107a5f19

--- src/main/java/edu/mssm/pharm/maayanlab/Enrichr/ListAdapter.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Enrichr/ListAdapter.java
@@ -1,3 +1,10 @@
+/**
+ * JsonSerializer object that helps serialize List
+ * 
+ * @author		Edward Y. Chen
+ * @since		02/25/2013 
+ */
+
 package edu.mssm.pharm.maayanlab.Enrichr;
 
 import java.lang.reflect.Type;
@@ -12,7 +19,7 @@ public class ListAdapter implements JsonSerializer<List> {
 	@Override
 	public JsonElement serialize(List list, Type type, JsonSerializationContext jsc) {
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("list_id", Shortener.encode(list.getListid()));
+		jsonObject.addProperty("list_id", Shortener.encode(list.getListid()));	// Don't show int count, show encoded version instead
 		jsonObject.addProperty("description", list.getDescription());
 		jsonObject.addProperty("passkey", list.getPasskey());
 		jsonObject.add("created", jsc.serialize(list.getCreated()));
